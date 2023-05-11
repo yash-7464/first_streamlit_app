5323c808a5b82625d7f3b93765371d6efcec2af1
yash-7464 <109007854+yash-7464@users.noreply.github.com>
2023-05-11 14:22:19 +0530

    Update streamlit_app.py
    added header and 3 menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit
 streamlit.title("Create first streamlit app")
 
 streamlit.header("Breakfast Menu!!1")
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
