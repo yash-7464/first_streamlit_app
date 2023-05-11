303ece7b10a8c8124efc319314dac24d5df9972a
yash-7464 <109007854+yash-7464@users.noreply.github.com>
2023-05-11 14:57:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 streamlit.title("Create first streamlit app")
 
 streamlit.header("🥣 Breakfast Menu!!1")
@@ -8,7 +10,7 @@ streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.dataframe(my_fruit_list)
