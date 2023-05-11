26d6f8fdce664df4d714fda9b625bfbe3ce35945
yash-7464 <109007854+yash-7464@users.noreply.github.com>
2023-05-11 14:25:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 streamlit.title("Create first streamlit app")
 
-streamlit.header("Breakfast Menu!!1")
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header("🥣 Breakfast Menu!!1")
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+   
 
