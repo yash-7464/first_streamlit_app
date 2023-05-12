cdef974137c8b0997c58f0766d93df94b76edf2e
yash-7464 <109007854+yash-7464@users.noreply.github.com>
2023-05-12 16:47:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load conatanis:")
 streamlit.dataframe(my_data_rows)
 
+streamlit.header("what fruit you like to add")
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
